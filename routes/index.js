dc5f1c1421921bedba60d05c66abf771d211636a
YourDevVince <vince.dev.help@gmail.com>
2025-10-21 15:56:48 -0500

    fix: added middleware unknown routes handler

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-
+const { NOT_FOUND } = require("http-status-codes");
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItem");
 
 router.use("/items", clothingItemRouter);
 router.use("/users", userRouter);
-
+router.use((req, res) => {
+  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
+});
 module.exports = router;
