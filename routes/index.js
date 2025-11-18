d3543f4b68e725496db9b12d39cf5254ef538fa3
YourDevVince <vince.dev.help@gmail.com>
2025-11-18 02:46:08 -0600

    test

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,16 @@ const clothingItemRouter = require("./clothingItem");
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 
-router.post('/signup', createUser);
-router.post('/signin', login);
+router.post("/signup", createUser);
+router.post("/signin", login);
 
 router.use(auth);
-router.use("/items", clothingItemRouter);
+
 router.use("/users", userRouter);
+router.use("/items", clothingItemRouter);
+
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
+
 module.exports = router;
