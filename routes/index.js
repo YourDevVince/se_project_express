f003f8aef634cd03c6d97bd0867a3015328d8063
YourDevVince <vince.dev.help@gmail.com>
2025-12-10 19:04:56 -0600

    Fix: clothing getClothingItem wasnt defined

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const { NOT_FOUND } = require("../utils/errors");
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItem");
 const { createUser, login } = require('../controllers/users');
+const { getClothingItems } = require('../controllers/clothingItem');
 const auth = require('../middlewares/auth');
 
 router.post("/signup", createUser);
