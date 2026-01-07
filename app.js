767554cc2b574f3e7a4c7eb329cb7649bd228a5e
YourDevVince <vince.dev.help@gmail.com>
2026-01-06 22:07:42 -0600

    feat: crash test

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(requestLogger);
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.use("/", mainRouter);
 app.use(errorLogger);
 
